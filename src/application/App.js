8245f85291c011ad9155953b7aa6ac97c4166761
gtrontz <gtrontz@gmail.com>
2022-10-19 14:12:26 -0700

    initial commit

---
+++ src/application/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "../presentation/components/footer/Footer";
+
+function App() {
+  return (
+    <div className="app">
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/presentation/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <span className="span1">email us: gotgrips@gmail.com</span>
+      <span className="span2">located in las vegas, nv</span>
+    </div>
+  );
+}
+
+export default Footer;
