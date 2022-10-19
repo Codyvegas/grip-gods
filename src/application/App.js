66b9604873d3ee7b62277434f758eb2311bf26f8
gtrontz <gtrontz@gmail.com>
2022-10-19 16:10:55 -0700

    updated footer component styling

--- src/application/App.js
+++ src/application/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "../presentation/components/footer/Footer";
+import Header from "../presentation/components/header/Header";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Footer />
     </div>
   );
