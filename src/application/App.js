847dae9de66cca5fda989c53eb6f38ac31d6495a
Codyvegas <codyjohnson2015@yahoo.com>
2022-10-19 16:46:33 -0700

    completed landing page

--- src/application/App.js
+++ src/application/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "../presentation/components/footer/Footer";
+import LandingPage from "../presentation/components/pages/landingPage/LandingPage";
 
 function App() {
   return (
     <div className="app">
+      <LandingPage />
       <Footer />
     </div>
   );
---
+++ src/presentation/components/pages/landingPage/constants.js
@@ -0,0 +1,2 @@
+export const phrase =
+  "the right re-grip all the time, every time, at all times, and always on time";
