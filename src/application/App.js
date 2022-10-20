11282f0b63248b04fb931a2eccc26a2d28af5e63
Codyvegas <codyjohnson2015@yahoo.com>
2022-10-20 15:53:28 -0700

    added black container box

--- src/application/App.js
+++ src/application/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import Header from "../presentation/components/header/Header";
 import Footer from "../presentation/components/footer/Footer";
 import LandingPage from "../presentation/components/pages/landingPage/LandingPage";
-import Header from "../presentation/components/header/Header";
 
 function App() {
   return (
     <div className="app">
-      <LandingPage />
       <Header />
+      <LandingPage />
       <Footer />
     </div>
   );
