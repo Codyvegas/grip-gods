3a3157af5a9832326b69a7d89924ee36a3c654d5
gtrontz <gtrontz@gmail.com>
2022-10-19 18:12:32 -0700

    completed header styling

--- src/application/App.js
+++ src/application/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../presentation/components/header/Header";
 import Footer from "../presentation/components/footer/Footer";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Footer />
     </div>
   );
---
+++ src/presentation/components/header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+import mainLogo from "../../../assets/icons/main_logo.jpg";
+
+function Header() {
+  return (
+    <div className="header">
+      <img src={mainLogo} alt=""></img>
+      <h1>grip gods</h1>
+      <div className="header-page-btns">
+        <span>follow us</span>
+        <span>drip god</span>
+        <span>about us</span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
