b47d5642e7c3bd1f16deb46f419bc281eb456ab3
Codyvegas <codyjohnson2015@yahoo.com>
2022-10-31 11:46:48 -0700

    style header button and adjust link for functionality

--- src/presentation/components/header/Header.js
+++ src/presentation/components/header/Header.js
@@ -1,17 +1,25 @@
 import React from "react";
 import "./Header.css";
-import mainLogo from "../../../assets/icons/main_logo.jpg";
+import mainLogo from "../../../assets/icons/CTJR_Logo.svg";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const onLogoCLick = () => {
+    console.log("logo was clicked");
+  };
+
   return (
     <div className="header">
-      <img src={mainLogo} alt=""></img>
+      <Link to="/">
+        <button className="logo-btn" onClick={onLogoCLick}>
+          <img src={mainLogo} alt=""></img>
+        </button>
+      </Link>
       <h1>grip gods</h1>
       <div className="header-page-btns">
-        <Link to="/follow-us">follow us</Link>
-        <Link to="/drip-gods">drip gods</Link>
         <Link to="about-us">about us</Link>
+        <Link to="/follow-us">follow us</Link>
+        <Link to="/drip-gods">drip god</Link>
       </div>
     </div>
   );
