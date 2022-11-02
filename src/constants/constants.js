aad82c531828fd93b7701d0e79e7a1bd75f968f4
Codyvegas <codyjohnson2015@yahoo.com>
2022-11-01 21:41:43 -0700

    create constant folder and allocate code from followUs.js

---
+++ src/constants/constants.js
@@ -0,0 +1,23 @@
+import instaIcon from "../../../../assets/icons/instagram_logo.jpg";
+import twitterIcon from "../../../../assets/icons/twitter_logo.jpg";
+import facebookIcon from "../../../../assets/icons/facebook_logo.jpg";
+import linkedInIcon from "../../../../assets/icons/linkedin_logo.jpg";
+
+export const socialLinks = [
+  {
+    img: instaIcon,
+    link: "https://www.instagram.com/gotgrips",
+  },
+  {
+    img: twitterIcon,
+    link: "https://www.twitter.com/gotgrips",
+  },
+  {
+    img: facebookIcon,
+    link: "https://www.facebook.com/gotgrips",
+  },
+  {
+    img: linkedInIcon,
+    link: "https://www.linkedin.com/gotgrips",
+  },
+];
--- src/presentation/components/pages/followUsPage/FollowUs.js
+++ src/presentation/components/pages/followUsPage/FollowUs.js
@@ -1,29 +1,7 @@
 import "./FollowUs.css";
-import instaIcon from "../../../../assets/icons/instagram_logo.jpg";
-import twitterIcon from "../../../../assets/icons/twitter_logo.jpg";
-import facebookIcon from "../../../../assets/icons/facebook_logo.jpg";
-import linkedInIcon from "../../../../assets/icons/linkedin_logo.jpg";
+import { socialLinks } from "../../../../constants/constants";
 
 function FollowUs() {
-  const socialLinks = [
-    {
-      img: instaIcon,
-      link: "https://www.instagram.com/gotgrips",
-    },
-    {
-      img: twitterIcon,
-      link: "https://www.twitter.com/gotgrips",
-    },
-    {
-      img: facebookIcon,
-      link: "https://www.facebook.com/gotgrips",
-    },
-    {
-      img: linkedInIcon,
-      link: "https://www.linkedin.com/gotgrips",
-    },
-  ];
-
   //react render array homework (dannys example)
 
   const renderSocialLinks = () => {
